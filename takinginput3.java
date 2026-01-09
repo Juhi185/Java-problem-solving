1ac34a08c0bfb8d4655990f1dfe06b5a8bd09219
Juhi185 <juhibhagat224@gmail.com>
2026-01-09 19:15:14 +0530

    Add files via upload

---
+++ takinginput3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/**
+ * takinginput3
+ */
+public class takinginput3 {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    String name = sc.next();
+    System.out.println("Enter your age: ");
+    int num_1 = sc.nextInt();
+    System.out.println("Enter your rollno: ");
+    int num_2 = sc.nextInt();
+    System.out.println("Enter your course: ");
+    String course = sc.next();
+    
+    }
+}
