3a6ee564c3fbaf6a3211317c720e14269f8a9baf
Juhi185 <juhibhagat224@gmail.com>
2026-01-09 19:04:07 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner.*;
+public class conditionaloperator{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        Int a = sc.nextInt();
+        
+    }
+}
+
+
