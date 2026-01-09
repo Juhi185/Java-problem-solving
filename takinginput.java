d0d2ac0216165ce6f0f6e184e342b43ba01dc439
Juhi185 <juhibhagat224@gmail.com>
2026-01-09 19:13:55 +0530

    Add files via upload

---
+++ takinginput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * takinginput
+ */
+public class takinginput {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the lucky number");
+        int num_1 = sc.nextInt();
+        System.out.println("The lucky number is: " +num_1);
+         
+    }
+}
