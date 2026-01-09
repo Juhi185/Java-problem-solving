351d812d7b6f3a6f530d10d8801111d9f61d91b2
Juhi185 <juhibhagat224@gmail.com>
2026-01-09 19:03:19 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class pattern{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of columns: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the number of rows: ");
+        int m = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+        
+    }
+
+
+}
