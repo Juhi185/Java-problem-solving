c4ecd14446146cbdba495a2b5635fdaf2a044e48
Juhi185 <juhibhagat224@gmail.com>
2026-01-09 19:14:33 +0530

    Add files via upload

---
+++ takinginput1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * takinginput
+ */
+public class takinginput1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String name = sc.next();
+        System.out.println(name);
+    }
+}
+
+
+
+
+        
