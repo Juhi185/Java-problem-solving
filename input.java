140956f39236abc12dc2cfeda75645b3f21ab0af
Juhi185 <juhibhagat224@gmail.com>
2026-01-09 19:00:38 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(string args[]){
+        Scanner sc = new Scanner (system.in);
+        string name = sc.nextline();
+        system.out.println(next);
+
+    }
+}
