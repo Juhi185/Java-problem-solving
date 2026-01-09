9a54352722c2bf2be0fe80c47b60bf85ef1130ee
Juhi185 <juhibhagat224@gmail.com>
2026-01-09 19:01:44 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+package com.apnacollege;
+import java.Util.*;
+public class  Main {
+    public static void main(string[] args){
+        Scanner sc = new Scanner (System.in);
+        String name = sc.next();
+        System.out.println(name);
+    }
+}
